5f9a538137c7b2f55c2f5674d57ddded2289a191
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-04 20:03:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,23 +30,23 @@ class TicTacToe
     @board[index] = current_player
   end
 
-  def position_taken?( location)
+  def position_taken?(location)
     @board[location] != " " && @board[location] != ""
   end
 
   def valid_move?( index)
-    index.between?(0,8) && !position_taken?(@board, index)
+    index.between?(0,8) && !position_taken?(index)
   end
 
-  def turn(board)
+  def turn
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-    if valid_move?(board, index)
+    if valid_move?( index)
       move(board, index, current_player(board))
       display_board(board)
     else
-      turn(board)
+      turn(@board)
     end
   end
 
